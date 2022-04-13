0dd08d278a078a1e740dc7e2f1b287e348f6b63f
Mauricio Mordecki <88162421+mauriciomordecki@users.noreply.github.com>
2022-04-13 14:44:48 -0300

    feat: now generating a model

---
+++ repository/__brick__/{{name.snakeCase()}}_repository/lib/src/{{#model}}models{{/model}}/{{model.snakeCase()}}.dart
@@ -0,0 +1,12 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+@immutable
+@JsonSerializable()
+class {{model.pascalCase()}} extends Equatable {
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+}
