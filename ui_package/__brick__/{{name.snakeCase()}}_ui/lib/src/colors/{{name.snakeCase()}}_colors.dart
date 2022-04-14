319453accaf01e5b02a807fc9ff39c5691025903
Mauricio Mordecki <88162421+mauriciomordecki@users.noreply.github.com>
2022-04-14 16:33:50 -0300

    ui_package now with more functionality

--- ui_package/__brick__/{{name.snakeCase()}}_ui/lib/src/colors/{{name.snakeCase()}}_colors.dart
+++ ui_package/__brick__/{{name.snakeCase()}}_ui/lib/src/colors/{{name.snakeCase()}}_colors.dart
@@ -6,4 +6,13 @@ abstract class {{name.pascalCase()}}Colors {
   static const Color primaryColor = Color(0xFF{{primary_color.upperCase()}});
 
   static const Color secondaryColor = Color(0xFF{{secondary_color.upperCase()}});
+{{#defines_tertiary_color}}
+  static const Color tertiaryColor = Color(0xFF{{tertiary_color.upperCase()}});
+{{/defines_tertiary_color}}
+{{#defines_white}}
+  static const Color white = Color(0xFF{{white.upperCase()}});
+{{/defines_white}}
+{{#defines_black}}
+  static const Color black = Color(0xFF{{black.upperCase()}});
+{{/defines_black}}
 }
--- ui_package/__brick__/{{name.snakeCase()}}_ui/lib/src/typography/{{name.snakeCase()}}_text_styles.dart
+++ ui_package/__brick__/{{name.snakeCase()}}_ui/lib/src/typography/{{name.snakeCase()}}_text_styles.dart
@@ -7,9 +7,50 @@ abstract class {{name.pascalCase()}}TextStyle {
   /// Headline 1 Text Style
   static const TextStyle headline1 = TextStyle(
     letterSpacing: 1.2,
-    fontSize: 60,
-    fontWeight: FontWeight.w600,
+    fontSize: {{headline1_size}},
+    fontWeight: FontWeight.w{{headline1_weight}},
     height: 1,
     package: '{{name.snakeCase()}}_ui',
   );
+{{#headline2}}
+
+  /// Headline 2 Text Style
+  static const TextStyle headline2 = TextStyle(
+    letterSpacing: 1.2,
+    fontSize: {{headline2_size}},
+    fontWeight: FontWeight.w{{headline2_weight}},
+    height: 1,
+    package: '{{name.snakeCase()}}_ui',
+  );
+{{/headline2}}
+  /// Body 1 text style
+  static const TextStyle body1 = TextStyle(
+    letterSpacing: 1.2,
+    fontSize: {{body1_size}},
+    fontWeight: FontWeight.w{{body1_weight}},
+    height: 1,
+    package: '{{name.snakeCase()}}_ui',
+  );
+  
+{{#body2}}
+  /// Body 2 Text Style
+  static const TextStyle body2 = TextStyle(
+    letterSpacing: 1.2,
+    fontSize: {{body2_size}},
+    fontWeight: FontWeight.w{{body2_weight}},
+    height: 1,
+    package: '{{name.snakeCase()}}_ui',
+  );
+{{/body2}}
+
+{{#button}}
+  /// Button Text Style
+  static const TextStyle button = TextStyle(
+    letterSpacing: 1.2,
+    fontSize: {{button_size}},
+    fontWeight: FontWeight.w{{button_weight}},
+    height: 1,
+    package: '{{name.snakeCase()}}_ui',
+  );
+{{/button}}
 }
